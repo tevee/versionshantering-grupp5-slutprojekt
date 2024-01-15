4c44c246496ec0c799661df244b305663bb906ee
tevee <brydiginte69@gmail.com>
2024-01-16 00:20:00 +0100

    added message functionality
    can post message, get message
    display posted messages on site reload
    
    message shows:
    - username
    - date
    -message

--- modules/display.js
+++ modules/display.js
@@ -20,3 +20,33 @@ export function displayGuest() {
     signInBtn.classList.add('show')
     signInBtn.classList.remove('hide')
 }
+
+export function getAndDisplayExistingMessages(messagesObj) {
+    const messageBoardEl = document.querySelector('#messageBoard')
+
+    for(const message in messagesObj) {
+        const uniqueMessage = messagesObj[message]
+
+        const div = document.createElement('div')
+        const h2 = document.createElement('h2')
+        const p = document.createElement('p')
+        h2.innerText = uniqueMessage.username
+        p.innerText = uniqueMessage.message
+        messageBoardEl.append(div)
+        div.append(h2,p)
+    }
+}
+
+// Temporär tills createAndAppend skapas
+export function displayMessage(uniqueMessage) {
+    const messageBoardEl = document.querySelector('#messageBoard')
+    const div = document.createElement('div')
+    const h2 = document.createElement('h2')
+    const p = document.createElement('p')
+    const secondP = document.createElement('p')
+    h2.innerText = uniqueMessage.username
+    p.innerText = uniqueMessage.date
+    secondP.innerText = uniqueMessage.message
+    messageBoardEl.append(div)
+    div.append(h2,p,secondP)
+}
