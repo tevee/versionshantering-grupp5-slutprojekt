26cd3af5f863e32ac5e9fc6373c8f00c40a47d4c
andjan3 <150336624+andjan3@users.noreply.github.com>
2024-01-16 11:29:38 +0100

    createAndAppend function, display function
    created createAndAppend function and implemented it to display function. Saved all "old" code in comments. Also did some small adjustment on navbar.

---
+++ modules/createElement.js
@@ -0,0 +1,8 @@
+export function createAndAppendElement(type, content, container) {
+	const element = document.createElement(type);
+	container.append(element);
+
+     element.innerText = content;
+
+	return element;
+}
