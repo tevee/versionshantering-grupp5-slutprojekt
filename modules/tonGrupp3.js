16d8d3bc94377bc76c8584820f443d561dd056f1
Ton <146097352+wiangin@users.noreply.github.com>
2024-01-21 20:52:12 +0100

    add alert message

---
+++ modules/tonGrupp3.js
@@ -0,0 +1,5 @@
+/*
+    Feature from Ton Group 3:
+    This particular feature will become visible when attempting to commit a message without being logged in."
+*/  
+alert('Please, log in to send message');
